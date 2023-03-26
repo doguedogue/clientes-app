e251954d5e9175b0105e446ffdcf9ce4553c12b7
Rafael Vilches <rvilches38@yahoo.com.mx>
2023-03-26 14:01:21 -0600

    add create service with BE

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { map } from 'rxjs/operators'
 
 @Injectable({
@@ -12,6 +12,8 @@ export class ClienteService {
 
   private urlEndPoint: string = 'http://localhost:8080/api/clientes';
 
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
@@ -20,5 +22,9 @@ export class ClienteService {
     return this.http.get(this.urlEndPoint).pipe(
       map( response => response as Cliente[] )
     );
+  }
+
+  create(cliente: Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders});
   }
 }
